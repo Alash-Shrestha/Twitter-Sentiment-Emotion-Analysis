4bd96f7b5375e9e8f6b18dbd22928b499b924bdb
Alash Shrestha <alashsth2019@gmail.com>
2024-07-08 08:39:59 +0545

    Fix urls path

--- sentiment_emotion_analysis/sentiment_or_emotion/urls.py
+++ sentiment_emotion_analysis/sentiment_or_emotion/urls.py
@@ -1,9 +1,8 @@
 from django.urls import path
-from django.conf.urls import url
 from . import views
 
 app_name = 'sentiment_or_emotion'
 
 urlpatterns = [
-    url(r'^$', views.choose_sentiment_or_emotion, name="choose_sentiment_or_emotion"),
+    path('', views.choose_sentiment_or_emotion, name="choose_sentiment_or_emotion"),
 ]
